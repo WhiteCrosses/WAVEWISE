f6fe1d6b08f0fd7ecbdbec89c91593b40f05fc8c
WhiteCrosses <szymon.modrzynski@gmail.com>
2023-05-10 16:12:11 +0200

    Cosmetic changes

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import time
 import adi
 
 import numpy as np
+import threading
+
 
 #Krzywe transmisyjne
 
-from superqt import QLabeledRangeSlider
+from superqt import QLabeledRangeSlider, QLabeledSlider
 
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qtagg import (
     FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
 from matplotlib.ticker import FuncFormatter
+from matplotlib import ticker
 import matplotlib.pyplot as plt
 import pandas as pd
 from PyQt5.QtCore import Qt, QEvent
@@ -86,12 +89,22 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.firstIteration()
           
     def firstIteration(self):
+
         self.x = np.arange(self.startFreq,self.bufferSize+self.startFreq)
         self.signal = self.x * 0
         self.imageArray = np.zeros((100,self.bufferSize))#change to variables later
         
         self._line = self._waterfall_ax.pcolorfast(np.reshape(self.imageArray,(100,self.bufferSize)),vmin=self.colorMeshMin,vmax=self.colorMeshMax)
         self._waterfall_ax.invert_yaxis()
+        ticks = np.array([0,1,2,3,4])
+        _ticks = ticks/4*self.sampleRate+self.startFreq
+        
+        
+        
+        self._waterfall_ax.locator_params(axis='both', nbins=5)
+        self._waterfall_ax.set_xticklabels(_ticks)
+        #self._waterfall_ax.xaxis.set_major_locator(ticker.FixedLocator(_ticks))
+        
         self._wave_ax.set_ylim(-100,0)
         self._wave_ax.set_ylim(0,100)
     def mainWidgetsInit(self):
@@ -145,14 +158,15 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
             formatter = FuncFormatter(millions)
             self._wave_ax.xaxis.set_major_formatter(formatter)
-        
+            
+            
+            
             self._wave_ax.plot(self.freq,self.data)
             
             self.renderPeaks()
             
             for i in self.recMarkerValues:
                 self._wave_ax.axvline(self.center_freq - (self.sampleRate/2) + (i/100*self.sampleRate), color='b')
-                print(i*100000)
             
             
             self._line.set_array(np.reshape(self.imageArray,(100,self.bufferSize)))
@@ -164,16 +178,14 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             self.peaks.sort(key=lambda x:x[0])
             for row in range(len(self.peaks)):
                 self.pRapTable.setItem(row,0,QtWidgets.QTableWidgetItem(str(self.peaks[row][0])))
-
-            
-                
-            
-            
+                 
     def renderPeaks(self):
-        x,y = self.checkPeaks()
-        buffer_size = self.pSetDistance / 2
-        peak_array = x
         
+        x,y = self.checkPeaks()
+        
+        buffer_size = self.pSetDistance / 2
+        peak_array = (self.center_freq - self.sampleRate/2) + self.sampleRate*x/self.bufferSize
+        self.peaksToSave = [peak_array,y]
         
         
         for n in peak_array:
@@ -183,7 +195,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                 if n+buffer_size > m[0] > n-buffer_size:
                     peakExists = True
 
-                    m = [n,True]
+                    m = [n,True,]
                     break
                 #print("\n")
             
@@ -200,8 +212,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                      
         for i in x:
             self._wave_ax.axvline(self.freq[i], color='r')
-            
-            
+              
     def getFrequencyArray(self):
         self.freq = np.fft.fftfreq(self.signal.size,d=1/self.sampleRate)
         self.freq = self.freq + self.center_freq
@@ -234,7 +245,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         
         #self.data = self.data + self.constantPart
         self.addToImageArray()
-        
+    
     def addToImageArray(self):    
         self.imageArray = np.roll(self.imageArray,1,axis=0)
         self.imageArray[0, :] = np.roll(self.data,int(self.data.size/2),axis=0)
@@ -325,16 +336,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetPlateauSize = self.pSetPlateauSizeSlider.value()
 
     def recChangeRecordingState(self):
-        
-        if self.isFile:
-            self.isRecording = not self.isRecording
-            self.file
-            if self.isRecording:
-                self.recButton.setText("Stop")
-            elif not self.isRecording:
-                self.recButton.setText("Start")
-        else:
-            self.selectFle()
+        self.recResolve()
       
     def eventFilter(self, object, event):
         if event.type() == QEvent.Enter:
@@ -351,7 +353,30 @@ class ApplicationWindow(QtWidgets.QMainWindow):
     def recRangeChange(self):
         self.recMarkerValues = self.recRangeSelector.value()
         
-            
+    def savePeaks(self):
+        with open('profiles2.csv', 'w', newline='') as file:
+            writer = csv.writer(file)            
+            writer.writerow(["Frequency [Hz]", "Power [dBm]"])
+            m=0
+            for i in self.peaksToSave[0]:
+                writer.writerow([i,self.peaksToSave[1]['peak_heights'][m]])
+                print([i,self.peaksToSave[1]['peak_heights'][m]])
+                m+=1
+            file.close()
+    
+    def saveWaterfall(self):
+        self._waterfall_ax.figure.savefig('waterfall.png')
+    
+    def recResolve(self):
+        match self.recType:
+            case 0:
+                self.savePeaks()
+            case 1:
+                self.saveWaterfall()
+            case 2:
+                self.savePeaks()
+                self.saveWaterfall()
+        
     def createWidgets(self):
         widget = QtWidgets.QWidget()
         self.menuBar = QtWidgets.QMenuBar(self)
@@ -371,7 +396,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.sampleRateBox.installEventFilter(self)
         
         self.centerFreqBox = QtWidgets.QLineEdit()
-        self.centerFreqBox.setInputMask("999.9999")
+        self.centerFreqBox.setInputMask("9999.9999")
         self.centerFreqBox.setText("100.0000") 
         self.centerFreqBox.returnPressed.connect(self.setCenterFreq)
         self.centerFreqBox.description = "Center frequency in MHz."
@@ -394,7 +419,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetHeightSlider.setMaximum(0)
         self.pSetHeightSlider.setSingleStep(5)
         self.pSetHeightSlider.valueChanged.connect(self.pSetHeightChange)
-        self.pSetHeightSlider.setToolTip("Required height of peaks.")
+        #self.pSetHeightSlider.setToolTip("Required height of peaks.")
         self.pSetHeightSlider.description = "Declare minimum height to peak be detected. Values ranging from -100 to 0."
         self.pSetHeightSlider.installEventFilter(self)
         
@@ -426,7 +451,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetTheresholdSlider.setMaximum(15)
         self.pSetTheresholdSlider.setSingleStep(1)
         self.pSetTheresholdSlider.valueChanged.connect(self.pSetTheresholdChange)
-        self.pSetTheresholdSlider.setToolTip("Required threshold of peaks, the vertical distance to its neighboring samples.")
+        #self.pSetTheresholdSlider.setToolTip("Required threshold of peaks, the vertical distance to its neighboring samples.")
         self.pSetTheresholdSlider.description = "Declare thereshold of the peak be detected."
         self.pSetTheresholdSlider.installEventFilter(self)
         
@@ -457,7 +482,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetDistanceSlider.setMaximum(100)
         self.pSetDistanceSlider.setSingleStep(1)
         self.pSetDistanceSlider.valueChanged.connect(self.pSetDistanceChange)
-        self.pSetDistanceSlider.setToolTip("Required minimal horizontal distance (>= 1) in samples between neighbouring peaks.\nSmaller peaks are removed first until the condition is fulfilled for all remaining peaks.")
+        #self.pSetDistanceSlider.setToolTip("Required minimal horizontal distance (>= 1) in samples between neighbouring peaks.\nSmaller peaks are removed first until the condition is fulfilled for all remaining peaks.")
         self.pSetDistanceSlider.description = "Required minimal horizontal distance (>= 1) in samples between neighbouring peaks.\nSmaller peaks are removed first until the condition is fulfilled for all remaining peaks."
         self.pSetDistanceSlider.installEventFilter(self)
         
@@ -479,7 +504,9 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetWidthSlider.setMaximum(100)
         self.pSetWidthSlider.setSingleStep(1)
         self.pSetWidthSlider.valueChanged.connect(self.pSetWidthChange)
-        self.pSetWidthSlider.setToolTip("Required width of peaks in samples.")
+        #self.pSetWidthSlider.setToolTip(f"Required width of peaks in percentage of all samples ({self.bufferSize}).")
+        self.pSetWidthSlider.description = f"Required width of peaks in percentage of all samples ({self.bufferSize})."
+        self.pSetWidthSlider.installEventFilter(self)
         
         #wlen
         self.pSetWlenSlider = QtWidgets.QSlider(Qt.Horizontal)
@@ -496,7 +523,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetRelHeightSlider.setTickInterval(10)
         self.pSetRelHeightSlider.setSingleStep(1)
         self.pSetRelHeightSlider.valueChanged.connect(self.pSetRelHeightChange)
-        self.pSetRelHeightSlider.setToolTip("Used for calculation of the peaks width, thus it is only used if width is given.")
+        #self.pSetRelHeightSlider.setToolTip("Used for calculation of the peaks width, thus it is only used if width is given.")
         
         
         #plateau size
@@ -506,19 +533,16 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.pSetPlateauSizeSlider.setTickInterval(10)
         self.pSetPlateauSizeSlider.setSingleStep(1)
         self.pSetPlateauSizeSlider.valueChanged.connect(self.pSetPlateauSizeChange)
-        self.pSetPlateauSizeSlider.setToolTip("Required size of the flat top of peaks in samples.")
+        #self.pSetPlateauSizeSlider.setToolTip("Required size of the flat top of peaks in samples.")
         
         self.statusBar = QtWidgets.QStatusBar()
         self.setStatusBar(self.statusBar)
         self.statusBar.showMessage("DUPA")
         
         self.recTypeChoice = QtWidgets.QComboBox()
-        self.recTypeChoice.addItems(("Full Range continuous",
-                                    "Narrow Range continuous",
-                                    "Full Range single frame",
-                                    "Narrow Range single frame",
-                                    "Peaks continuous",
-                                    "Peaks single frame"))
+        self.recTypeChoice.addItems(("Save peaks",
+                                    "Save waterfall",
+                                    "Save both"))
         self.recTypeChoice.currentIndexChanged.connect(self.recTypeChange)
         
         self.recRangeSelector = QLabeledRangeSlider(Qt.Horizontal)
@@ -530,12 +554,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         
         
         #Start/Stop button for recording to csv
-        self.recButton = QtWidgets.QPushButton("Start")
+        self.recButton = QtWidgets.QPushButton("Save!")
         self.recButton.clicked.connect(self.recChangeRecordingState)
         recButtonLayout = QtWidgets.QGridLayout()
         recButtonLayout.addWidget(self.recButton,0,0)
         recButtonLayout.addWidget(self.recTypeChoice,0,1)
-        recButtonLayout.addWidget(self.recRangeSelector,1,0,1,2)
+        #   recButtonLayout.addWidget(self.recRangeSelector,1,0,1,2)
         
         recordingBoxWidget = QtWidgets.QGroupBox("Recording")
         recordingBoxWidget.setLayout(recButtonLayout)
@@ -625,9 +649,10 @@ class TransmitWindow(QtWidgets.QWidget):
         super().__init__()
         self.frequencyTable = np.array(np.zeros((3,2)))
         self.buffer = 1024
+        self.gain = -50
         self.bufferAranged = np.array(np.arange(0,1024,1))
         self.mainLayout = QtWidgets.QVBoxLayout()
-        
+        self.done = True
         self._initPlotWidget()
         self._initFrequencyAddingWidget()
         
@@ -645,6 +670,37 @@ class TransmitWindow(QtWidgets.QWidget):
         self.ax.plot(self.bufferAranged, data, '*-')
         self.canvas.draw()
 
+    def gainChange(self):
+        self.gain = self.gainWidget.value()
+
+    def normalize(self,arr, t_min, t_max):
+        norm_arr = []
+        diff = t_max - t_min
+        diff_arr = max(arr) - min(arr)   
+        for i in arr:
+            temp = (((i - min(arr))*diff)/diff_arr) + t_min
+            norm_arr.append(temp)
+        return norm_arr
+
+    def runTransmit(self):
+        while(not self.done):
+            self.transmit()
+    
+    def stopTransmit(self):
+        self.done = True
+
+    def buttonClickedEvent(self):
+        if self.done:
+            self.done = False
+            self.runTransmit()
+            self.buttonWidget.setText("Stop")
+            
+        elif not self.done:
+            sdr = app.sdr
+            sdr.tx_destroy_buffer()
+            self.stopTransmit()
+            self.buttonWidget.setText("Transmit")
+            
     def _initFrequencyAddingWidget(self):
         self.tableWidget = QtWidgets.QTableWidget()
         self.buttonWidget = QtWidgets.QPushButton()
@@ -652,14 +708,27 @@ class TransmitWindow(QtWidgets.QWidget):
         self.tableWidget.cellChanged.connect(self.changeFrequencyArray)
         self.tableWidget.setRowCount(3)
         self.tableWidget.setColumnCount(1)
+        self.tableWidget.resize(self.tableWidget.sizeHint())
         
+        self.gainWidget = QLabeledSlider(Qt.Horizontal)
+        self.gainWidget.setFocusPolicy(Qt.StrongFocus)
+        self.gainWidget.setTickPosition(QtWidgets.QSlider.TicksBothSides)
+        self.gainWidget.setMinimum(-90)
+        self.gainWidget.setMaximum(-10)
+        self.gainWidget.setValue(-50)
+        self.gainWidget.setSingleStep(1)
+        self.gainWidget.valueChanged.connect(self.gainChange)
+        
+        self.buttonWidget.setText("Transmit")
         self.buttonWidget.clicked.connect(self.transmit)
         
-        frequencyAddingBox = QtWidgets.QWidget()
-        frequencyAddingLayout = QtWidgets.QHBoxLayout()
         
-        frequencyAddingLayout.addWidget(self.tableWidget)
-        frequencyAddingLayout.addWidget(self.buttonWidget)
+        frequencyAddingBox = QtWidgets.QWidget()
+        frequencyAddingLayout = QtWidgets.QGridLayout()
+        
+        frequencyAddingLayout.addWidget(self.tableWidget,0,0)
+        frequencyAddingLayout.addWidget(self.buttonWidget,1,0)
+        frequencyAddingLayout.addWidget(self.gainWidget,2,0)
         
         
         frequencyAddingBox.setLayout(frequencyAddingLayout)
@@ -676,6 +745,7 @@ class TransmitWindow(QtWidgets.QWidget):
             data = np.sin(w*self.bufferAranged*0.001)
             self.signal += data
         
+        self.signal = self.normalize(self.signal, -1, 1)
         self.figure.clear()
         self.ax = self.figure.add_subplot(111)
         self.ax.plot(self.bufferAranged,self.signal, '*-')
@@ -684,12 +754,14 @@ class TransmitWindow(QtWidgets.QWidget):
         #self.canvas.draw()
     
     def transmit(self):
-        print(app.bufferSize)
         if app.isPlutoRunning:
+            
             sdr = app.sdr
             sdr.tx_rf_bandwidth = int(app.sampleRate) # filter cutoff, just set it to the same as sample rate
             sdr.tx_lo = int(app.center_freq)
-            sdr.tx_hardwaregain_chan0 = -10 # Increase to increase tx power, valid range is -90 to 0 dB
+            if self.gain > 0:
+                return -1
+            sdr.tx_hardwaregain_chan0 = self.gain # Increase to increase tx power, valid range is -90 to 0 dB
 
             N = 1000 # number of samples to transmit at once
             t = np.arange(N)/app.sampleRate
@@ -699,14 +771,17 @@ class TransmitWindow(QtWidgets.QWidget):
                     samples = 0.5*np.exp(2.0j*np.pi*row[0]*1e6*t)
                 elif row[0] is not None:
                     samples += 0.5*np.exp(2.0j*np.pi*row[0]*1e6*t) # Simulate a sinusoid of 100 kHz, so it should show up at 915.1 MHz at the receiver
-                print(row[0])
+            
+            samples = self.normalize(samples, -1, 1)
             
             samples *= 2**14 # The PlutoSDR expects samples to be between -2^14 and +2^14, not -1 and +1 like some SDRs
             
             sdr.tx_cyclic_buffer = True # Enable cyclic buffers
-            sdr.tx(samples) # start transmitting  
-            for i in range (0, 10):
-                raw_data = sdr.rx()
+            print("transmiting!")
+            sdr.tx(samples)
+            for x in range(10):
+                sdr.rx()
+            print("transmited!")
             sdr.tx_destroy_buffer()
         else:
             print("Pluto not running!")
